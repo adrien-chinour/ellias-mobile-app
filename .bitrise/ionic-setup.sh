fc6b1a6c245d75072ff278e3d2b686bd404aa803
Adrien Chinour <git@chinour.fr>
2020-12-05 23:06:37 +0100

    update script

--- .bitrise/ionic-setup.sh
+++ .bitrise/ionic-setup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+echo "Install NPM dependencies"
 npm install --no-optional
 npm install -g @ionic/cli
 
+echo "Build ionic project"
+ionic build
+
+echo "Build Android & iOS project with Capacitor"
 ionic cap build
